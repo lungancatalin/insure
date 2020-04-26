b77818a2233472ccb1e341107741635cc5bd1b88
Catalin <lungancatalin@yahoo.com>
2020-04-26 22:43:50 +0300

    Updates

---
+++ precache-manifest.073a8f9be77e0fc4a112e706d0ad1bbb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c09f45cceaa226d4db7f8e6fb5bdf2b",
+    "url": "/insure/index.html"
+  },
+  {
+    "revision": "8f5b625193e996bbfc29",
+    "url": "/insure/static/css/main.fef066b4.chunk.css"
+  },
+  {
+    "revision": "1b1e2aa1856f56b77764",
+    "url": "/insure/static/js/2.21261bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insure/static/js/2.21261bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5b625193e996bbfc29",
+    "url": "/insure/static/js/main.2f17cb93.chunk.js"
+  },
+  {
+    "revision": "23479533af4050a02575",
+    "url": "/insure/static/js/runtime-main.c5947d41.js"
+  },
+  {
+    "revision": "b9dc0b5c6bd0cf4ddcd5ce9f3ad16897",
+    "url": "/insure/static/media/icon-affordable-prices.b9dc0b5c.svg"
+  },
+  {
+    "revision": "6fd0e32479e7d8af8e471d2a34244e93",
+    "url": "/insure/static/media/icon-facebook.6fd0e324.svg"
+  },
+  {
+    "revision": "d98c1990131df21b2dc25cd8c1382819",
+    "url": "/insure/static/media/icon-hamburger.d98c1990.svg"
+  },
+  {
+    "revision": "48a204c8175de66e9c8f1b9207cc77eb",
+    "url": "/insure/static/media/icon-instagram.48a204c8.svg"
+  },
+  {
+    "revision": "b264095b9d9862f3cc93d449508b65fd",
+    "url": "/insure/static/media/icon-people-first.b264095b.svg"
+  },
+  {
+    "revision": "275a65a2fef7bff622ba4fb6e7aa57eb",
+    "url": "/insure/static/media/icon-pinterest.275a65a2.svg"
+  },
+  {
+    "revision": "3cc5dc39b6297427126b6a7389e9b76d",
+    "url": "/insure/static/media/icon-snappy-process.3cc5dc39.svg"
+  },
+  {
+    "revision": "6eef62ff8487d0c9808c68e74d68d9c8",
+    "url": "/insure/static/media/icon-twitter.6eef62ff.svg"
+  },
+  {
+    "revision": "8ac69ec7c30fa6465fa05426a4b235f4",
+    "url": "/insure/static/media/image-intro-desktop.8ac69ec7.jpg"
+  },
+  {
+    "revision": "1bc452ac6f5856ef32a25e4519a4db91",
+    "url": "/insure/static/media/logo.1bc452ac.svg"
+  }
+]);
